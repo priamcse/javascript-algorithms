24bd77192cc18b9a12bc8b34f76266ff2ece3acc
priamcse <priamcse@outlook.com>
2022-07-15 16:41:43 +0600

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,12 @@
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
+};
+console.log(twoSum([2, 2, 7, 11, 15], 9));
