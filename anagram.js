a6089af20e2e9bc27473aad220914cb527b95c39
priamcse <priamcse@outlook.com>
2022-07-17 12:13:53 +0600

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -26,3 +26,30 @@ const result = validAnagram("car", "rat");
 console.log("====================================");
 console.log(result);
 console.log("====================================");
+
+// check valid anagram or not
+
+function validAnagram2(str1, str2) {
+  let obj1 = {};
+  let obj2 = {};
+  function strArray(str) {
+    return str.split("");
+  }
+  const str1Array = strArray(str1);
+  const str2Array = strArray(str2);
+  for (let item of str1Array) {
+    obj1[item] = (obj1[item] || 0) + 1;
+  }
+  for (let item of str2Array) {
+    obj2[item] = (obj2[item] || 0) + 1;
+  }
+  console.log(obj1, obj2);
+  // Check obj1 === obj2 or not
+  for (let key in obj1) {
+    console.log(obj1[key], obj2[key]);
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  return true;
+}
+const res = validAnagram2("texttwisttime", "timetwisttext");
+console.log(res);
